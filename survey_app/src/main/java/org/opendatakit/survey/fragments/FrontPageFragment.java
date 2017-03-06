35e514cb332a75943acba0212dedcffd248e9e44
Artur Fijał <afijal@soldevelo.com>
2017-03-06 17:12:06 +0100

    ODK-60: Attached main screen instances counters also some improvements connected to previous commit related to odk-60

--- survey_app/src/main/java/org/opendatakit/survey/fragments/FrontPageFragment.java
+++ survey_app/src/main/java/org/opendatakit/survey/fragments/FrontPageFragment.java
@@ -4,14 +4,20 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.opendatakit.consts.IntentConsts;
+import org.opendatakit.provider.FormsColumns;
+import org.opendatakit.provider.FormsProviderAPI;
+import org.opendatakit.provider.InstanceProviderAPI;
 import org.opendatakit.survey.R;
 import org.opendatakit.survey.activities.IOdkSurveyActivity;
 import org.opendatakit.survey.activities.MainMenuActivity;
@@ -25,7 +31,8 @@ public class FrontPageFragment extends Fragment implements Button.OnClickListene
     public static final int ID = R.layout.front_page;
 
     private OnFragmentInteractionListener mListener;
-    private Button send;
+    private Button subMenu;
+    private TextView counter;
 
     public interface OnFragmentInteractionListener {
         public void onFragmentInteraction(MainMenuActivity.ScreenList screen);
@@ -38,14 +45,18 @@ public class FrontPageFragment extends Fragment implements Button.OnClickListene
     @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                        Bundle savedInstanceState) {
         View view = inflater.inflate(ID, container, false);
-        send = (Button) view.findViewById(R.id.new_survey_btn);
-        send.setOnClickListener(this);
-        send = (Button) view.findViewById(R.id.submitted_btn);
-        send.setOnClickListener(this);
-        send = (Button) view.findViewById(R.id.in_progress_btn);
-        send.setOnClickListener(this);
-        send = (Button) view.findViewById(R.id.settings_btn);
-        send.setOnClickListener(this);
+        subMenu = (Button) view.findViewById(R.id.new_survey_btn);
+        subMenu.setOnClickListener(this);
+        subMenu = (Button) view.findViewById(R.id.submitted_btn);
+        subMenu.setOnClickListener(this);
+        subMenu = (Button) view.findViewById(R.id.in_progress_btn);
+        subMenu.setOnClickListener(this);
+        subMenu = (Button) view.findViewById(R.id.settings_btn);
+        subMenu.setOnClickListener(this);
+        counter = (TextView) view.findViewById(R.id.in_progress_counter);
+        counter.setText(Integer.toString(getFormInstancesCount("new_row")));
+        counter = (TextView) view.findViewById(R.id.submitted_counter);
+        counter.setText(Integer.toString(getFormInstancesCount("synced")));
         return view;
     }
 
@@ -99,5 +110,42 @@ public class FrontPageFragment extends Fragment implements Button.OnClickListene
                 IntentConsts.AppProperties.ACTIVITY_NAME));
         settingsIntent.setAction(Intent.ACTION_DEFAULT);
         this.startActivity(settingsIntent);
+    }
+
+    private int getFormInstancesCount(String arg){
+        int counter = 0;
+
+        Uri baseUri = Uri.withAppendedPath(FormsProviderAPI.CONTENT_URI, ((MainMenuActivity)getActivity()).getAppName());
+        Cursor c = null;
+        try {
+            c = getActivity().getContentResolver().query(baseUri, null, null, null, null);
+
+            if ( c.moveToFirst() ) {
+                int idxTableId = c.getColumnIndex(FormsColumns.TABLE_ID);
+                int idxFormVersion = c.getColumnIndex(FormsColumns.FORM_VERSION);
+                do {
+                    if(!c.getString(idxFormVersion).contains("sub")) {
+                        Uri formUri = Uri.withAppendedPath(InstanceProviderAPI.CONTENT_URI, ((MainMenuActivity)getActivity()).getAppName() + "/"
+                                + c.getString(idxTableId) );
+
+                        String[] whereArgs = new String[] {
+                                arg
+                        };
+
+                        Cursor f = getActivity().getContentResolver().query(formUri, null, "_sync_state=?", whereArgs, null);
+                        counter += f.getCount();
+
+                        if(f!=null) {
+                            f.close();
+                        }
+                    }
+                } while ( c.moveToNext());
+            }
+        } finally {
+            if ( c != null && !c.isClosed() ) {
+                c.close();
+            }
+        }
+        return counter;
     }
 }
